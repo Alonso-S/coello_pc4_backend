956b97616bc69485854022f1ab8aa2595b3248fd
Alonso-S <alonso.s2k5@gmail.com>
2025-07-03 23:03:06 -0500

    ✨ fix(server): configurar CORS para permitir solicitudes desde cualquier origen

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(
-  cors(),
+  cors({
+    origin: "*",
+  }),
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
